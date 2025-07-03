1d11fc0e22fa76dfa1c1b99a25bbd9dc469718e3
Aryaveer667766 <aryaveer0121@gmail.com>
2025-07-03 19:02:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -10,3 +10,25 @@ self.addEventListener('activate', event => {
 self.addEventListener('fetch', event => {
   // Optionally add caching logic here
 });
+// firebase-messaging-sw.js
+
+importScripts('https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/11.10.0/firebase-messaging.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBRwSrbSvrFAW5dTyQN0BoRHS3_6iLrV_E",
+  projectId: "homeworkbrothers-12345",
+  messagingSenderId: "77683184454",
+  appId: "1:77683184454:web:9b0bc1e8e5028943a3cb47",
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
